e1136ffd0d31efb5128df56b592e78364b7d6b67
Catherine Meyer <deathcat05@Catherines-MacBook-Pro.local>
2017-12-04 09:33:24 -0800

    successfully read from file, and stores all info into a vector.

--- main.cpp
+++ main.cpp
@@ -6,36 +6,39 @@
 #include <fstream>
 #include <cstring>
 #include <string>
+#include <vector> 
 //#include "FlatPageTable.hpp"
 using namespace std; 
 int main(int argc, char *argv[]) 
 {
 
 //FPTable *aTable = new FPTable; 
-
+// Variables needed for File input
 string fileName;
 ifstream inFile; 
 fileName = argv[1]; 
 inFile.open(fileName); 
+string inputInfo;  
+vector <string> tableInfo; 
+int vAddSize, pAddSize, pageSize; 
 if(!inFile.is_open())
 {
 	cout << "Error opening the file!" << endl;
 }	
 else
-{
-string vAddSize; 
-string  pAddSize;
-string pageSize; 
-char delimiter = ' ';
-	while(inFile.is_open())
+{ 
+	while(inFile.good())
 	{	
-		getline(inFile, vAddSize, delimiter);
-		cout << stoi(vAddSize) << endl;
-		if(EOF)
-		break;  
+		getline(inFile, inputInfo);  
+		tableInfo.push_back(inputInfo);   
 	}		
 }
   inFile.close(); 
-	return 0;
 
+for(int i = 0; i < tableInfo.size(); i++)
+{
+	cout << tableInfo[i] << endl; 
+}
+
+	return 0;
 }
